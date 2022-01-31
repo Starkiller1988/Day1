55ca1811766ab2db8dcaed2da5a59f90be4f623d
JavaKurs <javakurs@MacBook-Air-von-JavaKurs.local>
2022-01-31 15:56:00 +0100

    echo.sh

--- echo.sh
+++ echo.sh
@@ -4,6 +4,6 @@ echo Hallo $varname
 echo Wie ist dein Nachname?
 read varlastname  
 echo Hallo $varname $varlastname
-echo Wie alt bist du?
+echo Wie alt bist du $varname $varlastname?
 read varage
 echo $varage? Man bist du alt! 
