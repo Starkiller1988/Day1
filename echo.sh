c7e9dc2202900a438ad1440ce74b406cc4b9c37f
JavaKurs <javakurs@MacBook-Air-von-JavaKurs.local>
2022-01-31 16:02:05 +0100

    echo.sh

--- echo.sh
+++ echo.sh
@@ -7,3 +7,6 @@ echo Hallo $varname $varlastname
 echo Wie alt bist du $varname $varlastname?
 read varage
 echo $varage? Man bist du alt! 
+echo $varname $varlastname, arbeitest du?
+read varboolean
+echo $varboolean? Aaaah sehr cool! Dann viel Erfolg damit
